cc964d566d6e861fe7e46005689f0cc49786654f
GabrelBulz <gabibulz_hd@yahoo.com>
2018-07-06 14:13:40 +0300

    solved test file

--- client_sms/test/test_ConfigParser.py
+++ client_sms/test/test_ConfigParser.py
@@ -1,25 +1,30 @@
-import client_sms.ConfigParser
+import sys
+sys.path.append('..')
+import ConfigParser
 import unittest
 
 
 class Test_ConfigParser(unittest.TestCase):
 
     def test_parse_conf(self):
-        #test good config file
+        # test good config file
         config = ConfigParser.ConfigMachine('../conf.ini')
         self.assertEqual(config.parse_conf(), None)
 
-        #test with bad filename
+        # test with bad filename
         config.set_filename('fail_conf.ini')
-        with self.assertRaises(Exception): config.parse_conf()
+        with self.assertRaises(Exception):
+            config.parse_conf()
 
-        #test with missing ampq section
+        # test with missing ampq section
         config.set_filename('test_conf1.ini')
-        with self.assertRaises(Exception): config.parse_conf()
+        with self.assertRaises(Exception):
+            config.parse_conf()
 
-        #test with conf machine section
+        # test with conf machine section
         config.set_filename('test_conf1.ini')
-        with self.assertRaises(Exception): config.parse_conf()
+        with self.assertRaises(Exception):
+            config.parse_conf()
 
 
 if __name__ == 'main':
